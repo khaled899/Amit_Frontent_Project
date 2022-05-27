2ec878af363f82610cad3ce3bb8a314828ecc6e7
Khaled <101639047+khaled899@users.noreply.github.com>
2022-05-27 14:48:14 +0200

    done

---
+++ assests/index.js
@@ -0,0 +1,48 @@
+let novel = document.querySelectorAll('.novel');
+
+let btn = document.querySelectorAll('.btn')
+    .forEach(btn => btn
+        .addEventListener('click', function () {
+            novel.forEach(n => {
+                if (n.dataset.id != this.nextElementSibling.dataset.id) {
+                    n.classList.remove('active')
+                    console.log(this.id)
+                }
+            })
+            this.nextElementSibling.classList.toggle('active');
+        }));
+
+
+var bascis = document.querySelectorAll('.toggle_btn')
+    .forEach(btn => btn
+        .addEventListener('click', function () {
+
+                const boxes = document.getElementsByClassName('basics');
+
+                for (const box of boxes) {
+                    box.style.display = 'none';
+                }
+
+                document.querySelector(`table[data-id='${this.id}']`)
+                    .style.display = 'block';
+            }
+        )
+    );
+
+window.onscroll = function () {
+    scrollFunction()
+};
+
+var mybutton = document.getElementById("myBtn");
+
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500)
+        mybutton.style.display = "block";
+    else
+        mybutton.style.display = "none";
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
